414dca194ef2da05d2fb10ceb5ffef097e77cf7a
Tim <timawbi@gmail.com>
2021-04-30 00:02:48 +0900

    v

--- js/main.js
+++ js/main.js
@@ -2,22 +2,14 @@ new Vue({
     el: '#app',
     data: {
         results: [],
-        datenow: '',
-        datemonth:'Month',
-        datetime: '00:00',
-        dateweek: 'weekday',
-        weekday: Array(
-          "Sunday",
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday"),
-        city:'shinjuku',
+        dateDay: '',
+        dateDate:'Month Day',
+        dateTime: '00:00',
+        
+        city:'City',
         latlocation:'0',
         lonlocation:'0',
-        months: [
+        months:[
           'January',
           "February",
           "March",
@@ -30,58 +22,76 @@ new Vue({
           "October",
           "November",
           "December",
-        ]
-
+        ],
+        weekday: [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+          ],
+          unit:'Metric',
+          unitM:true,
+          loading:true,
+    },
+    beforeMount(){
+      
+        this.weather();
+        this.interval = setInterval(this.time, 1000)
+        this.interval = setInterval(this.weather, 100000) // 10secs
+        this.time()
+        this.weather()
     },
     methods: {
         time() {
             var d = new Date();
-           
-            this.datenow = d.getDate();
-            this.datetime =  d.getHours() +" : "
-            +d.getMinutes();
-            this.datemonth =  d.getMonth() +" "+ d.getDate();
-            this.dateweek =  this.weekday[d.getDate()];
+            this.dateDay = this.weekday[d.getDay()];
+            this.dateTime =  d.getHours() +" : "+d.getMinutes();
+            this.dateDate =  this.months[d.getMonth()] +" "+ d.getDate();
+            
         },
         
-        weather(){
-          axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${this.latlocation}&lon=${this.lonlocation}&units=metric&appid=1feaed6664354e68716f8f6fbb03ff5a`)
-          .then(response=>this.results = response.data)
-          .catch(error=>console.log(error))
+        async weather() {
+            this.getLocation();
+            const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=1feaed6664354e68716f8f6fbb03ff5a`);
+            this.results = await res.json();
         },
+
 
         getLocation() {
           if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(this.successz,this.errorz)
-          } else { 
-            alert('GEO NEEDED');
-          }
+          } 
         },
+
         successz(pos) {
-          var crd = pos.coords;
-          this.latlocation = crd.latitude;
-          this.lonlocation = crd.longitude;
-          this.cityName();
+          axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${pos.coords.latitude}&longitude=${ pos.coords.longitude}&localityLanguage=en`)
+          .then(response=>this.city = response.data)
+          .catch(console.log(e))
+        
         },
         errorz(e){
           alert('ERROR PERMISION NEEDED')
         },
-        cityName() {
-          axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${this.latlocation}&longitude=${this.lonlocation}&localityLanguage=en`)
-          .then(response=>this.city = response.data)
-          .catch(console.log('error'))
+       
+        changeUnit(){
+            this.unitM = !this.unitM;
         }
-    },
-    mounted() {
-      this.getLocation();
-      this.weather();
-      this.interval = setInterval(this.time, 1000)
-      this.interval = setInterval(this.weather, 50000) // 10secs
-      
-      
 
+    },
+ 
+    mounted() {
+      
+      this.loading= 0;
+      
     },
     beforeDestroy() {
       clearInterval(this.interval)
-    }
+      
+    },
+
+        
+    
 });
