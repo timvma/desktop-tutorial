048780bddf639e48b4a0b5b794a4b07f2146782a
timvma <timawbi@gmail.com>
2022-08-22 15:30:57 +0900

    update design

--- js/main.js
+++ js/main.js
@@ -1,97 +1,75 @@
-new Vue({
-    el: '#app',
-    data: {
-        results: [],
-        dateDay: '',
-        dateDate:'Month Day',
-        dateTime: '00:00',
-        
-        city:'City',
-        latlocation:'0',
-        lonlocation:'0',
-        months:[
-          'January',
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December",
-        ],
-        weekday: [
-            "Sunday",
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday"
-          ],
-          unit:'Metric',
-          unitM:true,
-          loading:true,
-          gpsNotFound:false,
-    },
-    beforeMount(){
-        this.weather();
-        this.interval = setInterval(this.time, 1000)
-        this.interval = setInterval(this.weather, 100000) // 10secs
-        this.time()
-        this.weather()
-    },
-    methods: {
-        time() {
-            var d = new Date();
-            this.dateDay = this.weekday[d.getDay()];
-            this.dateTime =  d.getHours() +" : "+d.getMinutes();
-            this.dateDate =  this.months[d.getMonth()] +" "+ d.getDate();
-            
-        },
-        
-        async weather() {
-            this.getLocation();
-            const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=1feaed6664354e68716f8f6fbb03ff5a`);
-            this.results = await res.json();
-        },
+const cityContainer = document.getElementById('city')
+const temperatureContainer = document.getElementById('temp')
+const sunMoon = document.getElementById('sun-moon')
 
 
-        getLocation() {
-          if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this.successz, this.errorz)
-          } 
-        },
+const d = new Date();
+let time = d.getHours();
 
-        successz(pos) {
-          axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${pos.coords.latitude}&longitude=${ pos.coords.longitude}&localityLanguage=en`)
-          .then(response=>this.city = response.data)
-          .catch(console.log(e))
-        
-        },
-        errorz(e){
-          this.gpsNotFound=true;
-        },
-       
-        changeUnit(){
-            this.unitM = !this.unitM;
-        }
+console.log( time )
 
-    },
- 
-    mounted() {
-      
-      this.loading= 0;
-      
-    },
-    beforeDestroy() {
-      clearInterval(this.interval)
-      
-    },
 
-        
-    
-});
+if(time == 0)  sunMoon.style.transform = 'rotate(270deg)'
+if(time == 1)  sunMoon.style.transform = 'rotate(285deg)'
+if(time == 2)  sunMoon.style.transform = 'rotate(300deg)'
+if(time == 3)  sunMoon.style.transform = 'rotate(315deg)'
+if(time == 4)  sunMoon.style.transform = 'rotate(330deg)'
+if(time == 5)  sunMoon.style.transform = 'rotate(345deg)'
+if(time == 6)  sunMoon.style.transform = 'rotate(0deg)'
+if(time == 7)  sunMoon.style.transform = 'rotate(15deg)'
+if(time == 8)  sunMoon.style.transform = 'rotate(30deg)'
+if(time == 9)  sunMoon.style.transform = 'rotate(45deg)'
+if(time == 10)  sunMoon.style.transform = 'rotate(60deg)'
+if(time == 11)  sunMoon.style.transform = 'rotate(75deg)'
+if(time == 12)  sunMoon.style.transform = 'rotate(90deg)'
+if(time == 13)  sunMoon.style.transform = 'rotate(105deg)'
+if(time == 14)  sunMoon.style.transform = 'rotate(120deg)'
+if(time == 15)  sunMoon.style.transform = 'rotate(135deg)'
+if(time == 16)  sunMoon.style.transform = 'rotate(150deg)'
+if(time == 17)  sunMoon.style.transform = 'rotate(165deg)'
+if(time == 18)  sunMoon.style.transform = 'rotate(180deg)'
+if(time == 19)  sunMoon.style.transform = 'rotate(195deg)'
+if(time == 20)  sunMoon.style.transform = 'rotate(210deg)'
+if(time == 21)  sunMoon.style.transform = 'rotate(225deg)'
+if(time == 22)  sunMoon.style.transform = 'rotate(240deg)'
+if(time == 23)  sunMoon.style.transform = 'rotate(255deg)'
+
+
+
+
+
+
+async function getIpAddress() {
+  const response = await fetch("https://ipgeolocation.abstractapi.com/v1/?api_key=3c47805d8d4344cba0a5a44669eb33ac");
+  const data = await response.json();
+  return data;
+}
+
+
+async function getWeather(lat,lon) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=1feaed6664354e68716f8f6fbb03ff5a`);
+  const data = await response.json();
+  return data;
+}
+
+
+getIpAddress()
+  .then(data => {
+      console.log(data);
+      cityContainer.textContent = data.city
+
+      getWeather(data.latitude,data.longitude)
+      .then(data => {
+          console.log(data);
+          console.log(data.main.temp);
+          temperatureContainer.textContent = Math.round(data.main.temp)
+      })
+      .catch(err => {
+          console.log(err);
+      })
+  })
+  .catch(err => {
+      console.log(err);
+})
+
+
